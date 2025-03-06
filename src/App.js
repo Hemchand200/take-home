2428766c79cf902e7482ff37d8c87114b9c4e515
Hemchand200 <60010510+Hemchand200@users.noreply.github.com>
2025-03-05 21:25:15 -0800

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/LoginPage";
+import Search from "./pages/SearchPage";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
+        <Route path="/search" element={isAuthenticated ? <Search /> : <Navigate to="/" />} />
+      </Routes>
+    </Router>
   );
 }
 
